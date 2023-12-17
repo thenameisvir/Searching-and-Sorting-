91783ef9fe502d572dc5379b605a830eeea2958c
thenameisivr <117061327+thenameisivr@users.noreply.github.com>
2023-12-17 16:21:14 +0530

    Add files via upload

---
+++ Bubble Sort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  int arr[10];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+   for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+      if(arr[i]>arr[j]){
+        swap(arr[i],arr[j]);
+      }
+    }
+  }
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  
+}
